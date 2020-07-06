3b8efb0e9df6c3ad5dc6bc11661362ba42061a05
Felipe Rodrigues <leepe.fr@gmail.com>
2020-07-06 15:54:24 -0300

    Págin de SignUp

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import SignIn from '../pages/Signin';
+import SignUp from '../pages/Signup';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route exact path="/signin" component={SignIn} />
+    <Route exact path="/signup" component={SignUp} />
   </Switch>
 );
 
