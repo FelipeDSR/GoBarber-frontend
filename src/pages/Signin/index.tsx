5989dfef301c2ef27b2809a0c854142fe694bd59
Felipe Rodrigues <leepe.fr@gmail.com>
2020-07-06 15:46:21 -0300

    Pagina SignIn

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,34 +1,49 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
-
-import { Container, Content, Background } from './styles';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
-const SignIn: React.FC = () => (
-  <>
-    <Container>
-      <Content>
-        <img src={logoImg} alt="GoBarber" />
-        <form>
-          <h1>Faça seu logon</h1>
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
-          <input type="email" placeholder="E-mail" />
+import { Container, Content, Background } from './styles';
 
-          <input type="password" placeholder="Senha" />
+const SignIn: React.FC = () => {
+  return (
+    <>
+      <Container>
+        <Content>
+          <img src={logoImg} alt="GoBarber" />
+          <form>
+            <h1>Faça seu logon</h1>
 
-          <button type="submit">Entrar</button>
+            <Input
+              name="email"
+              icon={FiMail}
+              type="email"
+              placeholder="E-mail"
+            />
 
-          <a href="/forgot">Esqueci minha senha</a>
-        </form>
-        <a href="/signup">
-          <FiLogIn />
-          Criar conta
-        </a>
-      </Content>
-      <Background />
-    </Container>
-  </>
-);
+            <Input
+              name="password"
+              icon={FiLock}
+              type="password"
+              placeholder="Senha"
+            />
+
+            <Button type="submit">Entrar</Button>
+
+            <a href="/forgot">Esqueci minha senha</a>
+          </form>
+          <a href="/signup">
+            <FiLogIn />
+            Criar conta
+          </a>
+        </Content>
+        <Background />
+      </Container>
+    </>
+  );
+};
 
 export default SignIn;
