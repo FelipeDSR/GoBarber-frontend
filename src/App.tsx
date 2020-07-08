7cb446bd3c0636d8eb7d7d10632b7a279117203d
Felipe Rodrigues <leepe.fr@gmail.com>
2020-07-08 15:39:29 -0300

    signOut

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './hooks/AuthContext';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 
