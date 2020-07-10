7eebd966a79cd4f37248e90382a5a3bcf8db03a6
Felipe Rodrigues <leepe.fr@gmail.com>
2020-07-10 12:18:35 -0300

    Finalizando a parte de SignUp

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useCallback } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
@@ -26,6 +26,8 @@ const SignIn: React.FC = () => {
   const { signIn } = useAuth();
   const { addToast } = useToast();
 
+  const history = useHistory();
+
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
       try {
@@ -46,9 +48,11 @@ const SignIn: React.FC = () => {
           password: data.password,
         });
 
+        history.push('/dashboard');
+
         addToast({
           type: 'success',
-          title: 'Login feito com sucesso',
+          title: 'Login realizado com sucesso',
         });
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
@@ -65,7 +69,7 @@ const SignIn: React.FC = () => {
         }
       }
     },
-    [signIn, addToast],
+    [signIn, addToast, history],
   );
   return (
     <>
--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,9 +1,13 @@
 import React, { useCallback, useRef } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+
+import api from '../../services/api';
+
+import { useToast } from '../../hooks/Toast';
 
 import getValidationErrors from '../../utils/getValidationErrors';
 
@@ -14,34 +18,61 @@ import Button from '../../components/Button';
 
 import { Container, Content, AnimationContainer, Background } from './styles';
 
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
 
-  const handleSubmit = useCallback(async data => {
-    try {
-      formRef.current?.setErrors({});
+  const history = useHistory();
 
-      const schema = Yup.object().shape({
-        name: Yup.string().required('Nome é obrigatório'),
-        email: Yup.string()
-          .email('Digite um email válido')
-          .required('E-mail é obrigatório'),
-        password: Yup.string().min(6, 'No mínimo 6 digitos'),
-      });
+  const handleSubmit = useCallback(
+    async (data: SignUpFormData) => {
+      try {
+        formRef.current?.setErrors({});
 
-      await schema.validate(data, {
-        abortEarly: false,
-      });
-    } catch (err) {
-      if (err instanceof Yup.ValidationError) {
-        const errors = getValidationErrors(err);
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Nome é obrigatório'),
+          email: Yup.string()
+            .email('Digite um email válido')
+            .required('E-mail é obrigatório'),
+          password: Yup.string().min(6, 'No mínimo 6 digitos'),
+        });
 
-        formRef.current?.setErrors(errors);
-      } else {
-        console.error('Outro erro');
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await api.post('/users', data);
+
+        history.push('/');
+
+        addToast({
+          type: 'success',
+          title: 'Cadastro realizado com sucesso',
+          description: 'Você já pode fazer seu logon no GoBarber',
+        });
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+        } else {
+          addToast({
+            type: 'error',
+            title: 'Erro no cadastro',
+            description:
+              'Ocorreu um erro ao fazer cadastro, cheque suas credenciais.',
+          });
+        }
       }
-    }
-  }, []);
+    },
+    [addToast, history],
+  );
   return (
     <>
       <Container>
